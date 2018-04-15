cd21a3d1d5e441e0534bc2033896ca587f6b5d83
alek <alek.merani+hex10@gmail.com>
2018-04-15 17:19:51 -0500

    Fix nav link underline and justify end

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import {AppBar, Toolbar, Button, withStyles} from 'material-ui';
 import { Link } from 'react-router-dom';
 
-const styles = {
+const styles: any = {
     root: {
-        flexGrow: 1
+        flexGrow: 1,
+    },
+    end: {
+        justifyContent: 'flex-end'
     }
 }
 
@@ -18,9 +21,17 @@ class HitchAppBar extends React.Component<any, any> {
         return (
             <div className={classes.root}>
                 <AppBar position="sticky">
-                    <Toolbar>
-                        <Button><Link to="/signup">SignUp</Link></Button>
-                        <Button><Link to="/login">LogIn</Link></Button>
+                    <Toolbar className={classes.end}>
+                        <Button>
+                            <Link style={{textDecoration: 'none', color: 'white'}} to="/signup">
+                                SignUp
+                            </Link>
+                        </Button>
+                        <Button>
+                            <Link style={{textDecoration: 'none', color: 'white'}} to="/login">
+                                LogIn
+                            </Link>
+                        </Button>
                     </Toolbar>
                 </AppBar>
             </div>
