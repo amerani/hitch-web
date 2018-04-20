fcdfac8829d2b0991d4d949d7e75382e9797513f
alek <alek.merani+hex10@gmail.com>
2018-04-19 22:00:12 -0500

    Style signup page

--- src/SignUpPage.tsx
+++ src/SignUpPage.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { TextField, Button } from 'material-ui';
 
 const mutation = gql`
     mutation signup($input: SignUpInput!) {
@@ -15,68 +16,113 @@ const mutation = gql`
 export default class SingUpPage extends React.Component<any, any> {
     constructor(props: any){
         super(props);
-        this.state = { userExists: false }
     }
 
-    async submitForm(event:any, mutate: any) {
-        event.preventDefault();
-        //TODO: validate before mutating
+    state: any = {
+        email: '',
+        password: '',
+        username: '',
+        firstname: '',
+        lastname: '',
+        error: false,
+        errorText: null
+    };
+    
+    handleChange = (name:any) => (event:any) => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
+    submitForm = async (mutate: any) => {
         try {
-            const res = await mutate({variables: {
-                input: {                
-                    email: event.target['email'].value,
-                    userName: event.target['userName'].value,
-                    password: event.target['password'].value,
-                    firstName: event.target['firstName'].value,
-                    lastName: event.target['lastName'].value
-                }
-            }})
+            const res = await mutate({
+                variables: {input: {
+                    email: this.state.email,
+                    password: this.state.password
+                }}
+            })
             const data = res.data.signup;
-            localStorage["HITCH_JWT"] = data.user.jwt;
+            localStorage['HITCH_JWT'] = data.user.jwt;
             this.props.history.push('/list');
         } catch (error) {
-            if(error.graphQLErrors.some((e: any) => e.message === 'email already exists')){
-                this.setState({ userExists: true });
-            }
-            console.log(error, error.graphQLErrors);            
+            if(error.graphQLErrors != null && 
+               error.graphQLErrors.some((e: any) => e.message === 'email already exists')){
+                this.setState({
+                    error: true,
+                    errorText: 'Account alreay exists. Please login.'
+                });
+            }      
+            else {
+                this.setState({
+                    error: true,
+                    errorText: 'Something went wrong'
+                });
+            }      
         }
-    }
-
-    loginRedirect() {
-        this.props.history.push('/login');
-    }
+    }    
 
     render() {
         return(
             <Mutation mutation={mutation}>{(mutate) => {
                 return(
                     <>
-                    <form onSubmit={(event) => this.submitForm(event, mutate)}>
-                        <label>
-                            Email
-                            <input type="text" name="email"/>
-                        </label><br/>
-                        <label>
-                            Password
-                            <input type="text" name="password"/>
-                        </label><br/>                     
-                        <label>
-                            UserName
-                            <input type="text" name="userName"/>
-                        </label><br/>                        
-                        <label>
-                            First Name
-                            <input type="text" name="firstName"/>
-                        </label><br/>                     
-                        <label>
-                            Last Name
-                            <input type="text" name="lastName"/>
-                        </label><br/>                                             
-                        <button type="submit">SignUp</button>
-                    </form>
-                    {this.state.userExists 
-                    ? <p> Account already exists <button onClick={() => this.loginRedirect()}>Login</button></p>
-                    : null }
+                        <form>
+                            <TextField
+                                error={this.state.error}                            
+                                id="email"
+                                label="Email"
+                                value={this.state.email}
+                                onChange={this.handleChange('email')}
+                                margin="normal"
+                                helperText={this.state.errorText}
+                                fullWidth={true}
+                                />
+                            <br/>
+                            <TextField
+                                error={this.state.error}
+                                id="password-input"
+                                label="Password"
+                                type="password"
+                                margin="normal"
+                                onChange={this.handleChange('password')}
+                                helperText={this.state.errorText}
+                                fullWidth={true}                                
+                                />
+                            <br/>
+                            <TextField
+                                error={this.state.error}
+                                id="username"
+                                label="Username"
+                                margin="normal"
+                                onChange={this.handleChange('username')}
+                                helperText={this.state.errorText}
+                                fullWidth={true}                                
+                                />
+                            <br/>
+                            <TextField
+                                error={this.state.error}
+                                id="firstname"
+                                label="First Name"
+                                margin="normal"
+                                onChange={this.handleChange('firstname')}
+                                helperText={this.state.errorText}
+                                fullWidth={true}                                
+                                />
+                            <br/>
+                            <TextField
+                                error={this.state.error}
+                                id="lastname"
+                                label="Last Name"
+                                margin="normal"
+                                onChange={this.handleChange('lastname')}
+                                helperText={this.state.errorText}
+                                fullWidth={true}                                
+                                />                                                                                                
+                            <br/>
+                            <br/>
+                            <Button variant="raised" onClick={() => this.submitForm(mutate)}>Submit</Button>
+                        </form>
                     </>
                 )
             }}
