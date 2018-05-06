088c2f446a68dce4fc58d224dae67319ee031331
alek <alek.merani+hex10@gmail.com>
2018-05-06 16:07:58 -0500

    Push message in list when received

--- src/Messages.tsx
+++ src/Messages.tsx
@@ -1,28 +1,36 @@
 import * as React from 'react';
 import { Subscription } from 'react-apollo';
 import gql from 'graphql-tag';
+import SearchItem, { fragment } from './SearchItem';
 
 const sub:any = gql`
   subscription tripCreated {
     tripCreated {
-      id
-      legs {
-        origin { city }
-      }
+      ...SearchTrip
     }
   }
+  ${fragment}
 `
 
 export default class Messages extends React.Component<any,any> {
+  messages: any = []
   render() {
     return (  
       <Subscription subscription={sub}>
       {({loading, data, error}) => {
+        if(!loading) {
+          this.messages.push(data.tripCreated)
+        }
         return (
           <>
             {loading 
               ? <p>Loading</p>
-              : JSON.stringify(data.tripCreated)
+              : this.messages.map((msg:any) =>
+                  <React.Fragment key={msg.id}>
+                    <h4>{msg.__typename} Created</h4>
+                    <SearchItem trip={msg} />
+                  </React.Fragment>
+                )
             }
           </>
         )
